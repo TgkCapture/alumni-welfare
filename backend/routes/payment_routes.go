5d6c869d78c1e3b8527fbb052c9f290720aae71b
TgkCapture <tawongakanyenda5@gmail.com>
2025-02-09 22:34:12 +0200

    FEAT: Added endpoint for payment history

--- backend/routes/payment_routes.go
+++ backend/routes/payment_routes.go
@@ -2,9 +2,15 @@ package routes
 
 import (
 	"github.com/TgkCapture/alumni-welfare/controllers"
+	"github.com/TgkCapture/alumni-welfare/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func PaymentRoutes(r *gin.Engine) {
-	r.POST("/pay", controllers.MakePayment)
+	auth := r.Group("/")
+	auth.Use(middleware.AuthMiddleware())
+	{
+		auth.POST("/pay", controllers.MakePayment)
+		auth.GET("/payments", controllers.GetPaymentHistory)
+	}
 }

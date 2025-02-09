9efee609f4f6676c94a8b35c949266a4e35b7b9a
TgkCapture <tawongakanyenda5@gmail.com>
2025-02-09 22:13:02 +0200

    FEAT: added more fields

--- backend/models/user.go
+++ backend/models/user.go
@@ -4,6 +4,8 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name  string `json:"name"`
-	Email string `json:"email" gorm:"unique"`
+	Name     string    `json:"name"`
+	Email    string    `json:"email" gorm:"unique"`
+	Password string    `json:"password"`
+	Payments []Payment `gorm:"foreignKey:UserID"`
 }

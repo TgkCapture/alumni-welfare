491c61907a507a571ef85fc0ec5e841b2ebe402f
TgkCapture <tawongakanyenda5@gmail.com>
2025-02-10 12:00:06 +0200

    FEAT: added the routes/ endpoints

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Container, CssBaseline, Typography } from "@mui/material";
+import { Navigate } from "react-router-dom";
 import PaymentPage from "./pages/PaymentPage";
 import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -13,6 +16,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/payment" element={<PaymentPage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Container>
     </Router>

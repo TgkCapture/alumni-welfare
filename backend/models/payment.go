ea496e1c3e29a44cffc85b8b0cd5ae4c62918e19
TgkCapture <tawongakanyenda5@gmail.com>
2025-02-10 15:29:56 +0200

    FEAT: Store API transaction ID and Status

--- backend/models/payment.go
+++ backend/models/payment.go
@@ -4,8 +4,10 @@ import "gorm.io/gorm"
 
 type Payment struct {
 	gorm.Model
-	UserID uint   `json:"user_id"`
-	Name   string `json:"name"`
-	Amount int    `json:"amount"`
-	Month  int    `json:"month"`
+	UserID        uint   `json:"user_id"`
+	Name          string `json:"name"`
+	Amount        int    `json:"amount"`
+	Month         int    `json:"month"`
+	TransactionID string `json:"transaction_id"`
+	Status        string `json:"status"`
 }

c03d26bbcaf60270dbf388b2ec1c485ef8c4336a
TgkCapture <tawongakanyenda5@gmail.com>
2025-02-14 15:35:35 +0200

    Feat: updated the user model

--- backend/models/user.go
+++ backend/models/user.go
@@ -6,8 +6,11 @@ import (
 
 type User struct {
 	gorm.Model
-	Name     string    `json:"name"`
-	Email    string    `json:"email" gorm:"unique"`
-	Password string    `json:"-"`
-	Payments []Payment `gorm:"foreignKey:UserID"`
+	FirstName    string    `json:"first_name"`
+	LastName     string    `json:"last_name"`
+	Name         string    `json:"name"`
+	Email        string    `json:"email" gorm:"unique"`
+	MobileNumber string    `json:"mobile_number"`
+	Password     string    `json:"-"`
+	Payments     []Payment `gorm:"foreignKey:UserID"`
 }
